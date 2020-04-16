6859319345be80f867f786a5968b6eff74b7ca1f
funbiscuit <kokurinsv@gmail.com>
2020-04-16 20:48:52 +0300

    Update .appveyor.yml and mainwindow.cpp

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <QPushButton>
 
 #include "mainwindow.h"
 
@@ -7,6 +8,9 @@ MainWindow::MainWindow()
 {
     setMinimumSize(600, 400);
     setWindowTitle("Cmake CI Test");
+    auto train_button = new QPushButton(this);
+    train_button->setText(tr("something"));
+    setCentralWidget(train_button);
 }
 
 MainWindow::~MainWindow()
